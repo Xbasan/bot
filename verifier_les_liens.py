86d938e2b6e1adcd87fe29627c9933531500224c
Xbasan <121888389+Xbasan@users.noreply.github.com>
2024-01-22 19:14:41 +0300

    Update verifier_les_liens.py

--- verifier_les_liens.py
+++ verifier_les_liens.py
@@ -6,7 +6,7 @@ from random import choice
 from string import ascii_letters, digits
 import telebot
 
-bot = telebot.TeleBot('5473853379:AAGy2mEkaKNlLmOS2r4kIZxbVVxhC-LxnQQ')
+bot = telebot.TeleBot('Ключ бота')
 
 headers = {"User-Agent":
                "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)"}
