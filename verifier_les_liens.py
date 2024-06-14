a65c5dcc22aaa1df05f197e803f26dd9506af8f3
hasan <121888389+Xbasan@users.noreply.github.com>
2024-06-14 20:31:15 +0300

    Update verifier_les_liens.py

--- verifier_les_liens.py
+++ verifier_les_liens.py
@@ -36,8 +36,6 @@ def start_message(message):
                     bot.send_message(message.from_user.id, expression)
 
 
-bot.polling(none_stop=True, interval=0)
-
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
 
